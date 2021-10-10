2e3161241d98a1a30fb0ffa8ac3a7d606869f62a
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-10-10 22:13:53 +0100

    feat(forge): set platform factory & init player manager

--- forge/src/main/java/com/envyful/ultimate/poke/builder/forge/UltimatePokeBuilderForge.java
+++ forge/src/main/java/com/envyful/ultimate/poke/builder/forge/UltimatePokeBuilderForge.java
@@ -1,7 +1,15 @@
 package com.envyful.ultimate.poke.builder.forge;
 
 import com.envyful.api.config.yaml.YamlConfigFactory;
+import com.envyful.api.forge.gui.factory.ForgeGuiFactory;
+import com.envyful.api.forge.player.ForgeEnvyPlayer;
+import com.envyful.api.forge.player.ForgePlayerManager;
+import com.envyful.api.gui.factory.GuiFactory;
+import com.envyful.api.player.EnvyPlayer;
+import com.envyful.api.player.PlayerManager;
 import com.envyful.ultimate.poke.builder.forge.config.GuiConfig;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraftforge.fml.client.GuiBackupFailed;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
@@ -20,10 +28,13 @@ public class UltimatePokeBuilderForge {
 
     private static UltimatePokeBuilderForge instance;
 
+    private PlayerManager<ForgeEnvyPlayer, EntityPlayerMP> playerManager = new ForgePlayerManager();
+
     private GuiConfig guiConfig;
 
     @Mod.EventHandler
     public void onServerStarting(FMLPreInitializationEvent event) {
+        GuiFactory.setPlatformFactory(new ForgeGuiFactory());
         instance = this;
 
         this.loadConfig();
@@ -44,6 +55,10 @@ public class UltimatePokeBuilderForge {
 
     public static UltimatePokeBuilderForge getInstance() {
         return instance;
+    }
+
+    public PlayerManager<ForgeEnvyPlayer, EntityPlayerMP> getPlayerManager() {
+        return this.playerManager;
     }
 
     public GuiConfig getGuiConfig() {

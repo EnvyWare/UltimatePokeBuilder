81bc32ce7f4d7722ff32e5b08adee6e3a3910bd5
Daniel Norris <dnorris443@gmail.com>
2023-01-10 04:26:13 +0000

    chore(gradle): bump version to 2.1.6

--- forge/src/main/java/com/envyful/ultimate/poke/builder/forge/UltimatePokeBuilderForge.java
+++ forge/src/main/java/com/envyful/ultimate/poke/builder/forge/UltimatePokeBuilderForge.java
@@ -36,7 +36,7 @@ import java.sql.SQLException;
 )
 public class UltimatePokeBuilderForge {
 
-    public static final String VERSION = "2.1.5";
+    public static final String VERSION = "2.1.61";
 
     private static UltimatePokeBuilderForge instance;
 

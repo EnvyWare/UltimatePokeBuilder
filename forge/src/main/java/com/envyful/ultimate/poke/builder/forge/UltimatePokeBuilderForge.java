4dc3fb56695df88de0b28bf400a8d63c13935c73
Daniel Norris <dnorris443@gmail.com>
2023-07-08 20:40:32 +0100

    del(git): remove old libraries

--- forge/src/main/java/com/envyful/ultimate/poke/builder/forge/UltimatePokeBuilderForge.java
+++ forge/src/main/java/com/envyful/ultimate/poke/builder/forge/UltimatePokeBuilderForge.java
@@ -36,7 +36,7 @@ import java.sql.SQLException;
 )
 public class UltimatePokeBuilderForge {
 
-    public static final String VERSION = "2.1.7";
+    public static final String VERSION = "2.1.8";
 
     private static UltimatePokeBuilderForge instance;
 

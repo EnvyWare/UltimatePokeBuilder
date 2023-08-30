89e67f859730c720054334ab84b473bffe685ed4
Daniel Norris <dnorris443@gmail.com>
2023-08-30 10:19:23 +0100

    feat(forge20): port player data

---
+++ forge20/src/main/java/com/envyful/ultimate/poke/builder/forge/eco/player/PokeBuilderAttribute.java
@@ -0,0 +1,70 @@
+package com.envyful.ultimate.poke.builder.forge.eco.player;
+
+import com.envyful.api.forge.player.ForgePlayerManager;
+import com.envyful.api.forge.player.attribute.AbstractForgeAttribute;
+import com.envyful.api.player.save.attribute.DataDirectory;
+import com.envyful.ultimate.poke.builder.forge.UltimatePokeBuilderForge;
+import com.envyful.ultimate.poke.builder.forge.config.PokeBuilderQueries;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@DataDirectory("config/players/UltimatePokeBuilder/")
+public class PokeBuilderAttribute extends AbstractForgeAttribute<UltimatePokeBuilderForge> {
+
+    private double tokens = 0;
+
+    public PokeBuilderAttribute(UltimatePokeBuilderForge manager, ForgePlayerManager playerManager) {
+        super(manager, playerManager);
+    }
+
+    public double getTokens() {
+        return this.tokens;
+    }
+
+    public void setTokens(double tokens) {
+        this.tokens = tokens;
+    }
+
+    @Override
+    public void load() {
+        if (this.manager.getDatabase() == null) {
+            return;
+        }
+
+        try (Connection connection = this.manager.getDatabase().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(PokeBuilderQueries.LOAD_USER)) {
+            preparedStatement.setString(1, this.parent.getUuid().toString());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (!resultSet.next()) {
+                this.tokens = this.manager.getConfig().getDefaultTokens();
+                return;
+            }
+
+            this.tokens = resultSet.getInt("balance");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void save() {
+        if (this.manager.getDatabase() == null) {
+            return;
+        }
+
+        try (Connection connection = this.manager.getDatabase().getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(PokeBuilderQueries.UPDATE_CREATE_USER)) {
+            preparedStatement.setString(1, this.parent.getUuid().toString());
+            preparedStatement.setInt(2, (int)this.tokens);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
